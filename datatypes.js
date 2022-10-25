d2b8c4b5fa998d1f31101078ba321828ba6db0ad
Brijrajsinh Gohil <61138673+Brijraj80@users.noreply.github.com>
2022-10-25 12:03:14 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+var username =  'Raj'
+var age = 23
+var school = "Hillwoods"
+var ismarried = false
+
+console.log("This is String Datatype:",username,school)
+console.log("This is Number Datatype:",age)
+console.log("This is Boolean Datatype:",ismarried)
